90f2c2dedcad454f09fd6fe250fb54aba8097c1f
olvrcrls <dev.olivercarlos@gmail.com>
2023-02-09 08:25:47 +0800

    feat(dashboard): Add button and change color of buttons to primary blue.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -9,7 +10,47 @@ module.exports = {
     ],
 
     theme: {
+        colors: {
+            'primary-blue': '#001C3D',
+            yellow: '#FFC20E'
+        },
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+        },
         extend: {
+            colors: {
+                inherit: colors.inherit,
+                current: colors.current,
+                transparent: colors.transparent,
+                black: colors.black,
+                white: colors.white,
+                slate: colors.slate,
+                gray: colors.gray,
+                zinc: colors.zinc,
+                neutral: colors.neutral,
+                stone: colors.stone,
+                red: colors.red,
+                orange: colors.orange,
+                amber: colors.amber,
+                lime: colors.lime,
+                green: colors.green,
+                emerald: colors.emerald,
+                teal: colors.teal,
+                cyan: colors.cyan,
+                sky: colors.sky,
+                blue: colors.blue,
+                yellow: colors.yellow,
+                indigo: colors.indigo,
+                violet: colors.violet,
+                purple: colors.purple,
+                fuchsia: colors.fuchsia,
+                pink: colors.pink,
+                rose: colors.rose,
+            },
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
